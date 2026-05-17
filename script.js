24268e7f9650c89c886b45ef94f3e6c94be0b9cb
VisuAllArt <kawanyuri348@gmail.com>
2026-05-17 03:16:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,29 @@ for(let i = 0; i < 50; i++){
   particles.appendChild(particle);
 
 }
+
+/* PREÇO PERSONALIZADO */
+
+const slider = document.getElementById('slider');
+const quantity = document.getElementById('quantity');
+const price = document.getElementById('price');
+
+slider.addEventListener('input', () => {
+
+  const fotos = slider.value;
+
+  quantity.innerText = fotos;
+
+  let valor = fotos * 3.5;
+
+  if(fotos >= 10){
+    valor = fotos * 4.5;
+  }
+
+  if(fotos >= 20){
+    valor = fotos * 3.5;
+  }
+
+  price.innerText = `R$${Math.round(valor)}`;
+
+});
